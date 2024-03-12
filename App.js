dc7e04a82f7b613753eb9ed20b4c275c7103dc7c
Mohammed Rahimuddin Farooqui <mohammedrahimuddinfarooqui30@gmail.com>
2024-03-12 16:56:39 +0530

    Form2,3 creacted

---
+++ App.js
@@ -0,0 +1,24 @@
+
+import './Style.css'
+import FormWorks4 from './WorkingWithForm/FormWorks4';
+// import FormWorks3 from './WorkingWithForm/FormWorks3';
+
+// import FormWorks from './WorkingWithForm/FormWorks';
+// import FormWorks2 from './WorkingWithForm/FormWorks2';
+
+
+
+function App() {
+  
+  return (
+    <div className='App container bg-dark text-light cx-main p-4'>
+    {/* <FormWorks/> */}
+    {/* <FormWorks2/> */}
+    {/* <FormWorks3/> */}
+    <FormWorks4/>
+ 
+    </div>
+  );
+}
+
+export default App;
